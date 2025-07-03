e2f1ecd46980427e359df96490bd518103bd79e2
dzraka <muhdzakirakamaulana10@gmail.com>
2025-07-03 15:35:42 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
-    return view('siswa.index');
+    return view('welcome');
 });
 
 
